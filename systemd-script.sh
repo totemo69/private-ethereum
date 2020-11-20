f16faa83b1cf17c576fc4140c86b2163fdd3a776
nrmn-yasc <nrmn101@outlook.com>
2020-11-20 13:40:13 +0800

    update sh

--- systemd-script.sh
+++ systemd-script.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-if [ ! -L /lib/systemd/system/bootnode.service ]; then
-        echo "linking systemd bootnode.service..."
-        /bin/ln -s /datadisk/private-ethereum/bootnode/bootnode.service /lib/systemd/system
-        echo "...done!"
-else
-        echo "bootnode.service already exist.."
-fi
-if [ ! -L /lib/systemd/system/rpcnode.service ]; then
-        echo "linking systemd rpcnode.service..."
-        /bin/ln -s /datadisk/private-ethereum/rpcnode/rpcnode.service /lib/systemd/system
-        echo "...done!"
-else
-        echo "rpcnode.service already exist.."
-fi
-if [ ! -L /lib/systemd/system/minernode.service ]; then
-        echo "linking systemd minernode.service..."
-        /bin/ln -s /datadisk/private-ethereum/minernode/minernode.service /lib/systemd/system
-        echo "...done!"
-else
-        echo "minernode.service already exist.."
-fi
+
+echo "linking systemd bootnode.service..."
+/bin/cp -f /datadisk/private-ethereum/bootnode/bootnode.service /etc/systemd/system
+echo "...done!"
+
+echo "linking systemd rpcnode.service..."
+/bin/cp -f /datadisk/private-ethereum/rpcnode/rpcnode.service /etc/systemd/system
+echo "...done!"
+        
+echo "linking systemd minernode.service..."
+/bin/cp -f /datadisk/private-ethereum/minernode/minernode.service /etc/systemd/system
+echo "...done!"
+
+echo "executing daemon reload.."
+sudo systemctl daemon-reload
+echo "...done!"
+
+echo "enabling at startup.."
+sudo systemctl enable bootnode.service
+sudo systemctl enable rpcnode.service
+sudo systemctl enable minernode.service
+echo "...done!"
