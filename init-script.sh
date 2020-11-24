c38174bd5477e8c4b0ab5856cbad237a45e897d9
nrmn-yasc <nrmn101@outlook.com>
2020-11-24 13:19:57 +0800

    updates

--- init-script.sh
+++ init-script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 if [ ! -d /datadisk/private-ethereum/bootnode/data/geth/chaindata ]; then
     echo "/datadisk/private-ethereum/bootnode/geth/chaindata, running 'geth init'..."
-    /usr/bin/geth init --datadir /datadisk/private-ethereum/bootnode/data /datadisk/private-ethereum/genesis.json >> /datadisk/private-ethereum/bootnode/eth.log
+    /usr/bin/geth init --datadir /datadisk/private-ethereum/bootnode/data /datadisk/private-ethereum/genesis.json
     echo "...done!"
 else
     echo "geth init unsuccessful on bootnode, please check logs.."
 fi    
 if [ ! -d /datadisk/private-ethereum/rpcnode/data/geth/chaindata ]; then
     echo "/datadisk/private-ethereum/rpcnode/geth/chaindata, running 'geth init'..."
-    /usr/bin/geth init --datadir /datadisk/private-ethereum/rpcnode/data /datadisk/private-ethereum/genesis.json >> /datadisk/private-ethereum/rpcnode/eth.log
+    /usr/bin/geth init --datadir /datadisk/private-ethereum/rpcnode/data /datadisk/private-ethereum/genesis.json
     echo "...done!"
 else
     echo "geth init unsuccessful on rpcnode, please check logs.."
 fi    
 if [ ! -d /datadisk/private-ethereum/minernode/data/geth/chaindata ]; then
     echo "/datadisk/private-ethereum/minernode/geth/chaindata, running 'geth init'..."
-    /usr/bin/geth init --datadir /datadisk/private-ethereum/minernode/data /datadisk/private-ethereum/genesis.json >> /datadisk/private-ethereum/minernode/eth.log
+    /usr/bin/geth init --datadir /datadisk/private-ethereum/minernode/data /datadisk/private-ethereum/genesis.json
     echo "...done!"
 else
     echo "geth init unsuccessful on minernode, please check logs.."
