e05ece1d6f6dd2917d92766731f3c6610f6bd78c
nrmn-yasc <nrmn101@outlook.com>
2020-12-02 18:21:04 +0800

    update script

--- addpeer.sh
+++ addpeer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "starting the 3 nodes.."
+sudo systemctl start bootnode.service
+sudo systemctl start rpcnode.service
+sudo systemctl start minernode.service
+echo "..done!"
+
 echo "adding peer to bootnode.."
 /usr/bin/geth attach /datadisk/private-ethereum/bootnode/data/geth.ipc --exec "admin.addPeer('$1')"
 echo "..creating static-nodes.json.."
@@ -33,4 +39,10 @@ $bootenode
 EOF
 /bin/sed -i s/?discport=0//g /datadisk/private-ethereum/minernode/data/geth/static-nodes.json
 /bin/cat /datadisk/private-ethereum/minernode/data/geth/static-nodes.json
+echo "..done!"
+
+echo "restarting the 3 nodes.."
+sudo systemctl restart bootnode.service
+sudo systemctl restart rpcnode.service
+sudo systemctl restart minernode.service
 echo "..done!"
